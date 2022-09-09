087450edc4f959375f352e01533376071f02ed62
Amaranese <32201849+Amaranese@users.noreply.github.com>
2022-09-09 11:14:56 +0200

    Create Member.kt

---
+++ College Students Communication App/app/src/main/java/com/alejandro.maranes/tuchatapplication/models/Member.kt
@@ -0,0 +1,9 @@
+package com.alejandro.maranes.tuchatapplication.models
+
+class Member(
+    var userId: String? = null,
+    var groupId: String? = null,
+    var code: String? = null,
+    var dateAdded: String? = null
+) {
+}
