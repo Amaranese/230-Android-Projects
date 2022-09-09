a3844e7e138a5b6b45e52bb980a8dbf0e5ead923
Amaranese <32201849+Amaranese@users.noreply.github.com>
2022-09-09 11:16:48 +0200

    Create ChatEntries.kt

---
+++ College Students Communication App/app/src/main/java/com/alejandro.maranes/tuchatapplication/sqlitedatabase/queries/ChatEntries.kt
@@ -0,0 +1,19 @@
+package com.alejandro.maranes.tuchatapplication.sqlitedatabase.queries
+
+import com.alejandro.maranes.tuchatapplication.sqlitedatabase.tables.ChatsTable
+
+class ChatEntries {
+    companion object{
+        var CHAT_SQL_ENTRIES = "CREATE TABLE ${ChatsTable.TABLE_NAME} (" +
+                "${ChatsTable.COLUMN_NAME_ID} INTEGER," +
+                "${ChatsTable.COLUMN_NAME_CHAT_ID} TEXT," +
+                "${ChatsTable.COLUMN_NAME_DATE} TEXT," +
+                "${ChatsTable.COLUMN_NAME_GROUPID} TEXT," +
+                "${ChatsTable.COLUMN_NAME_MESSAGE} TEXT," +
+                "${ChatsTable.COLUMN_NAME_TIME} TEXT," +
+                "${ChatsTable.COLUMN_NAME_USERID} TEXT," +
+                "${ChatsTable.COLUMN_NAME_IMAGE} TEXT)"
+
+        var CHAT_SQL_DROP = "DROP TABLE IF EXISTS ${ChatsTable.TABLE_NAME}"
+    }
+}
