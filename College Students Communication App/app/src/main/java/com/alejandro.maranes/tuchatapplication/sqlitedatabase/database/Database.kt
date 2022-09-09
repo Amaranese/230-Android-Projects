37240a9c8c5af2dca01b4703bf2c8588aa3efabb
Amaranese <32201849+Amaranese@users.noreply.github.com>
2022-09-09 11:14:50 +0200

    Create Database.kt

---
+++ College Students Communication App/app/src/main/java/com/alejandro.maranes/tuchatapplication/sqlitedatabase/database/Database.kt
@@ -0,0 +1,9 @@
+package com.alejandro.maranes.tuchatapplication.sqlitedatabase.database
+
+class Database {
+    companion object{
+        //database
+        const val DATABASE_NAME = "chatapplication.db"
+        const val DATABASE_VERSION = 10
+    }
+}
