c8b1c2a949bd6c8eac01d9d256ab5b0d9708bc29
Amaranese <32201849+Amaranese@users.noreply.github.com>
2022-09-09 11:17:18 +0200

    Create ChatsTable.kt

---
+++ College Students Communication App/app/src/main/java/com/alejandro.maranes/tuchatapplication/sqlitedatabase/tables/ChatsTable.kt
@@ -0,0 +1,15 @@
+package com.alejandro.maranes.tuchatapplication.sqlitedatabase.tables
+
+class ChatsTable {
+    companion object{
+        const val TABLE_NAME = "chatstable"
+        const val COLUMN_NAME_ID = "id"
+        const val COLUMN_NAME_CHAT_ID = "chatid"
+        const val COLUMN_NAME_USERID = "userid"
+        const val COLUMN_NAME_MESSAGE = "message"
+        const val COLUMN_NAME_TIME = "time"
+        const val COLUMN_NAME_DATE = "date"
+        const val COLUMN_NAME_GROUPID = "groupid"
+        const val COLUMN_NAME_IMAGE = "image"
+    }
+}
