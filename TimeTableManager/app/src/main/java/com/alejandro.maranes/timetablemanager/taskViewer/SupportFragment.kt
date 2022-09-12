f0391c4c6b5fc4d62772bcadae57f7152b7c3618
Amaranese <32201849+Amaranese@users.noreply.github.com>
2022-09-12 10:58:45 +0200

    Create SupportFragment.kt

---
+++ TimeTableManager/app/src/main/java/com/alejandro.maranes/timetablemanager/taskViewer/SupportFragment.kt
@@ -0,0 +1,35 @@
+package com.alejandro.maranes.timetablemanager.taskViewer
+
+import android.os.Bundle
+import android.text.method.LinkMovementMethod
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import com.alejandro.maranes.timetablemanager.R
+
+class SupportFragment : Fragment() {
+
+    private lateinit var support:TextView
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_support, container, false)
+    }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        support=view.findViewById(R.id.supportText)
+        support.movementMethod= LinkMovementMethod.getInstance()
+
+    }
+
+
+}
