beba862cd4f6e22da9d78e4fe13108a940d02539
Amaranese <32201849+Amaranese@users.noreply.github.com>
2022-09-14 11:23:37 +0200

    Create Trains.kt

---
+++ Train-Food-Ordering-App/app/src/main/java/com/alejandro.maranes/TrainFoodOrder/data/model/Trains.kt
@@ -0,0 +1,9 @@
+package com.alejandro.maranes.TrainFoodOrder.data.model
+
+import androidx.annotation.DrawableRes
+
+data class Trains(
+    @DrawableRes val imageResourceId: Int,
+     val name :Int,
+     val route :Int
+)
