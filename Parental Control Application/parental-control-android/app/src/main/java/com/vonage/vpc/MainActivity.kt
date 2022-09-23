055da832a7f5bf7359c87da9d8de821e6d14eb0a
Amaranese <32201849+Amaranese@users.noreply.github.com>
2022-09-23 11:42:12 +0200

    Create MainActivity.kt

---
+++ Parental Control Application/parental-control-android/app/src/main/java/com/vonage/vpc/MainActivity.kt
@@ -0,0 +1,109 @@
+package com.vonage.vpc
+
+import android.app.Activity
+import android.app.role.RoleManager
+import android.content.Intent
+import android.os.Build
+import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import com.vonage.vpc.databinding.ActivityMainBinding
+
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private val roleAcquireRequestCode = 4378
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+    }
+
+    override fun onStart() {
+        super.onStart()
+        updateUI()
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        Log.d(
+            "MainActivity",
+            "onActivityResult requestCode: $requestCode resultCode:$resultCode data:$data"
+        )
+        when(requestCode){
+            roleAcquireRequestCode->{
+                if (resultCode == Activity.RESULT_OK){
+                    updateUI()
+                }
+            }
+        }
+    }
+
+    private fun updateUI() {
+        binding.stateCallProxy.isEnabled = false
+        binding.enableRoleCallProxy.isEnabled = false
+
+        binding.phoneNumberInput.setText(getOwnerPhoneNumber(this))
+        if (!isRedirection()) {
+            binding.enableRoleCallProxy.isEnabled = true
+            binding.enableRoleCallProxy.setOnClickListener {
+                setOwnerPhoneNumber(binding.phoneNumberInput.text.toString(), this)
+                roleAcquire(RoleManager.ROLE_CALL_REDIRECTION)
+                setRedirectState(true, this)
+                updateUI()
+            }
+            return
+        }
+
+        binding.stateCallProxy.isEnabled = true
+        binding.stateCallProxy.isChecked = getRedirectState(this)
+        binding.stateCallProxy.setOnClickListener {
+            setRedirectState(binding.stateCallProxy.isChecked, this)
+            updateUI()
+        }
+    }
+
+    // Is our app registered as the call redirection service
+    private fun isRedirection(): Boolean {
+        return isRoleHeldByApp(RoleManager.ROLE_CALL_REDIRECTION)
+    }
+
+    private fun isRoleHeldByApp(roleName: String): Boolean {
+        var roleManager: RoleManager?
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+            roleManager = getSystemService(RoleManager::class.java)
+            return roleManager.isRoleHeld(roleName)
+        }
+        return false
+    }
+
+    private fun roleAcquire(roleName: String) {
+        var roleManager: RoleManager?
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+            if (roleAvailable(roleName)) {
+                roleManager = getSystemService(RoleManager::class.java)
+                val intent =
+                    roleManager.createRequestRoleIntent(roleName)
+                startActivityForResult(intent, roleAcquireRequestCode)
+            } else {
+                Toast.makeText(
+                    this,
+                    "Redirection call with role in not available",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+        }
+    }
+
+    private fun roleAvailable(roleName: String): Boolean {
+        var roleManager: RoleManager?
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+            roleManager = getSystemService(RoleManager::class.java)
+            return roleManager.isRoleAvailable(roleName)
+        }
+        return false
+    }
+}
